de3ec954194a69483c075c5f8dec8f99e6670b39
abhishek <abhishek.solanki1110@gmail.com>
2018-06-17 09:13:19 +0530

    add GET todos id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
@@ -23,6 +23,8 @@ app.post('/todos', (req,res) => {
   });
 });
 
+
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
@@ -30,6 +32,25 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+      if(!todo){
+        res.status(402).send();
+      }
+      res.send({todo});
+    },(e) => {
+      res.status(404).send();
+    });
+});
+
+
+
 
 
 
